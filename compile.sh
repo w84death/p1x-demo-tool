d4a22a693ce59304a3a83d6ab3713a5bd4e4089b
Krzysztof Jankowski <krzysztof.jankowski@beffio.com>
2023-04-17 18:36:11 +0200

    Linux demo pack

--- compile.sh
+++ compile.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "Compiling for smallest size..."
-g++ main.cpp -o demo -lglfw -lGLEW -lGL && upx --best demo
-echo "Done; Run ./demo"
+g++ main.cpp -o demo -lglfw -lGLEW -lGL && upx --best demo && cat demo pass_fragment_shader.glsl pass_vertex_shader.glsl fragment_shader.glsl vertex_shader.glsl > demoLinux
+ls -l demoLinux
+echo "Done; Run ./demoLinux"
