6f637d718d6ff163247a199f1c9de130baa0f42e
Krzysztof Jankowski <krzysztof.jankowski@beffio.com>
2023-04-20 01:00:12 +0200

    alsa

--- alsa.cpp
+++ alsa.cpp
@@ -1,35 +1,3 @@
-#include <cstdio>
-#include <math.h>
-#include <alsa/asoundlib.h>
-#define TSF_IMPLEMENTATION
-#include "tsf.h"
-
-//This is a minimal SoundFont with a single loopin saw-wave sample/instrument/preset (484 bytes)
-const static unsigned char MinimalSoundFont[] =
-{
-	#define TEN0 0,0,0,0,0,0,0,0,0,0
-	'R','I','F','F',220,1,0,0,'s','f','b','k',
-	'L','I','S','T',88,1,0,0,'p','d','t','a',
-	'p','h','d','r',76,TEN0,TEN0,TEN0,TEN0,0,0,0,0,TEN0,0,0,0,0,0,0,0,255,0,255,0,1,TEN0,0,0,0,
-	'p','b','a','g',8,0,0,0,0,0,0,0,1,0,0,0,'p','m','o','d',10,TEN0,0,0,0,'p','g','e','n',8,0,0,0,41,0,0,0,0,0,0,0,
-	'i','n','s','t',44,TEN0,TEN0,0,0,0,0,0,0,0,0,TEN0,0,0,0,0,0,0,0,1,0,
-	'i','b','a','g',8,0,0,0,0,0,0,0,2,0,0,0,'i','m','o','d',10,TEN0,0,0,0,
-	'i','g','e','n',12,0,0,0,54,0,1,0,53,0,0,0,0,0,0,0,
-	's','h','d','r',92,TEN0,TEN0,0,0,0,0,0,0,0,50,0,0,0,0,0,0,0,49,0,0,0,34,86,0,0,60,0,0,0,1,TEN0,TEN0,TEN0,TEN0,0,0,0,0,0,0,0,
-	'L','I','S','T',112,0,0,0,'s','d','t','a','s','m','p','l',100,0,0,0,86,0,119,3,31,7,147,10,43,14,169,17,58,21,189,24,73,28,204,31,73,35,249,38,46,42,71,46,250,48,150,53,242,55,126,60,151,63,108,66,126,72,207,
-		70,86,83,100,72,74,100,163,39,241,163,59,175,59,179,9,179,134,187,6,186,2,194,5,194,15,200,6,202,96,206,159,209,35,213,213,216,45,220,221,223,76,227,221,230,91,234,242,237,105,241,8,245,118,248,32,252
-};
-static tsf* TinySoundFont;
-
-// g++ -o sine_wave alsa.cpp -lasound -lm
-int audio_callback(snd_pcm_t *pcm, const snd_pcm_channel_area_t *areas, snd_pcm_uframes_t frames) {
-    // SampleCount = (len / (2 * sizeof(short)));
-    tsf_render_short(TinySoundFont, (short*)pcm, frames, 0);
-    return 0;
-}
-
-
-
 int main()
 {
     int err;
@@ -75,4 +43,45 @@ snd_pcm_close(handle);
 
 return 0;
 
+=======
+#include <alsa/asoundlib.h>
+//g++ alsa.cpp -o alsa -lasound -ldl -lm
+long loops;
+int rc;
+int size;
+snd_pcm_t *handle;
+snd_pcm_hw_params_t *params;
+unsigned int val;
+int dir;
+snd_pcm_uframes_t frames;
+char *buffer;
+
+int main() {
+
+    rc = snd_pcm_open(&handle, "default", SND_PCM_STREAM_PLAYBACK, 0);
+    snd_pcm_hw_params_alloca(&params);
+    snd_pcm_hw_params_any(handle, params);
+    snd_pcm_hw_params_set_access(handle, params,
+    SND_PCM_ACCESS_RW_INTERLEAVED);
+    snd_pcm_hw_params_set_format(handle, params,
+    SND_PCM_FORMAT_S16_LE);
+    snd_pcm_hw_params_set_channels(handle, params, 2);
+    val = 44100;
+    snd_pcm_hw_params_set_rate_near(handle, params,&val, &dir);
+    frames = 32;
+    snd_pcm_hw_params_set_period_size_near(handle,params, &frames, &dir);
+    rc = snd_pcm_hw_params(handle, params);
+    snd_pcm_hw_params_get_period_size(params, &frames,&dir);
+    size = frames * 4; /* 2 bytes/sample, 2 channels */
+    buffer = (char *) malloc(size);
+    snd_pcm_hw_params_get_period_time(params,&val, &dir);
+    loops = 5000000 / val;
+    while (loops > 0) {
+        loops--;
+        rc = read(0, buffer, size);
+        rc = snd_pcm_writei(handle, buffer, frames);
+    }
+    snd_pcm_drain(handle);
+    snd_pcm_close(handle);
+    free(buffer);
 }
