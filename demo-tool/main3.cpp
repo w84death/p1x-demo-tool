0ca12e9319b50736718a99417864e291497572ef
Krzysztof Jankowski <krzysztof.jankowski@beffio.com>
2023-05-10 16:26:24 +0200

    V3 window+opengl init

--- demo-tool/main3.cpp
+++ demo-tool/main3.cpp
@@ -1,124 +1,155 @@
 /*
-* -----10--------20--------30--------40--------50--------60--------70-------80
-*
-* P1X DEMO TOOL V3
-* by w84death^P1X
-*
-* https://github.com/w84death/p1x-demo-tool
-* (c) 2023.04
-*
-* -----10--------20--------30--------40--------50--------60--------70-------80
-*/
-
-#include <iostream>
-#include <GL/gl.h>
-#include <GL/glx.h>
-#include <X11/X.h>
+ * -----10--------20--------30--------40--------50--------60--------70-------80
+ *
+ * P1X DEMO TOOL V3
+ * by w84death^P1X
+ *
+ * https://github.com/w84death/p1x-demo-tool
+ * (c) 2023.05
+ *
+ * -----10--------20--------30--------40--------50--------60--------70-------80
+ */
 #include <X11/Xlib.h>
-
-const char* fragment_source =
-#include "glsl/fragment_shader.glsl"
-;
-const char* vertex_source =
-#include "glsl/vertex_shader.glsl"
-;
-
+#include <GL/glx.h>
 
 /*
-* -----10--------20--------30--------40--------50--------60--------70-------80
-*/
+ * -----10--------20--------30--------40--------50--------60--------70-------80
+ */
+
+typedef GLuint (*PFNGLCREATESHADERPROC)(GLenum type);
+typedef void (*PFNGLCOMPILESHADERPROC)(GLuint shader);
+typedef GLuint (*PFNGLCREATEPROGRAMPROC)(void);
+typedef void (*PFNGLATTACHSHADERPROC)(GLuint program, GLuint shader);
+typedef void (*PFNGLLINKPROGRAMPROC)(GLuint program);
+typedef void (*PFNGLUSEPROGRAMPROC)(GLuint program);
+typedef void (*PFNGLGENBUFFERSPROC)(GLsizei n, GLuint* buffers);
+typedef void (*PFNGLBINDBUFFERPROC)(GLenum target, GLuint buffer);
+typedef void (*PFNGLBUFFERDATAPROC)(GLenum target, GLsizeiptr size, const void* data, GLenum usage);
+typedef GLint (*PFNGLGETATTRIBLOCATIONPROC)(GLuint program, const GLchar* name);
+typedef void (*PFNGLVERTEXATTRIBPOINTERPROC)(GLuint index, GLint size, GLenum type, GLboolean normalized, GLsizei stride, const void* pointer);
+typedef void (*PFNGLENABLEVERTEXATTRIBARRAYPROC)(GLuint index);
+typedef void (*PFNGLDISABLEVERTEXATTRIBARRAYPROC)(GLuint index);
+
+PFNGLCREATESHADERPROC glCreateShader = NULL;
+PFNGLSHADERSOURCEPROC glShaderSource = NULL;
+PFNGLCOMPILESHADERPROC glCompileShader = NULL;
+PFNGLCREATEPROGRAMPROC glCreateProgram = NULL;
+PFNGLATTACHSHADERPROC glAttachShader = NULL;
+PFNGLLINKPROGRAMPROC glLinkProgram = NULL;
+PFNGLUSEPROGRAMPROC glUseProgram = NULL;
+PFNGLGENBUFFERSPROC glGenBuffers = NULL;
+PFNGLBINDBUFFERPROC glBindBuffer = NULL;
+PFNGLBUFFERDATAPROC glBufferData = NULL;
+PFNGLGETATTRIBLOCATIONPROC glGetAttribLocation = NULL;
+PFNGLVERTEXATTRIBPOINTERPROC glVertexAttribPointer = NULL;
+PFNGLENABLEVERTEXATTRIBARRAYPROC glEnableVertexAttribArray = NULL;
+PFNGLDISABLEVERTEXATTRIBARRAYPROC glDisableVertexAttribArray = NULL;
+
+/*
+ * -----10--------20--------30--------40--------50--------60--------70-------80
+ */
+
+const char* vertexShaderSource = "attribute vec2 position;"
+"void main() {"
+"  gl_Position = vec4(position, 0.0, 1.0);"
+"}";
+
+const char* fragmentShaderSource = "void main() {"
+"  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);"
+"}";
+
+/*
+ * -----10--------20--------30--------40--------50--------60--------70-------80
+ */
+
 int main() {
-    int width = 1280/2;
-    int height = 800/2;
-    const char *title = "P1X DEMO TOOL V3";
+  Display *display = XOpenDisplay(NULL);
+  int screen = DefaultScreen(display);
+  Window rootWindow = RootWindow(display, screen);
+
+  GLint glxAttribs[] = {GLX_RGBA, GLX_DOUBLEBUFFER, GLX_DEPTH_SIZE, 24, GLX_STENCIL_SIZE, 8, None};
+  XVisualInfo *visualInfo = glXChooseVisual(display, screen, glxAttribs);
+
+  Window window = XCreateWindow(display, rootWindow, 10, 10, 800, 600, 0, visualInfo->depth, InputOutput, visualInfo->visual, 0, NULL);
+  XMapWindow(display, window);
+  XFlush(display);
+
+  GLXContext glxContext = glXCreateContext(display, visualInfo, NULL, GL_TRUE);
+  glXMakeCurrent(display, window, glxContext);
 
   /*
-  * ---10--------20--------30--------40--------50--------60--------70-------80
-  */
-    // Open an X11 display connection
-    Display *display = XOpenDisplay(nullptr);
+    * ---10--------20--------30--------40--------50--------60--------70-------80
+    */
+  glCreateShader = (PFNGLCREATESHADERPROC)glXGetProcAddress((const GLubyte*)"glCreateShader");
+  glShaderSource = (PFNGLSHADERSOURCEPROC)glXGetProcAddress((const GLubyte*)"glShaderSource");
+  glCompileShader = (PFNGLCOMPILESHADERPROC)glXGetProcAddress((const GLubyte*)"glCompileShader");
+  glCreateProgram = (PFNGLCREATEPROGRAMPROC)glXGetProcAddress((const GLubyte*)"glCreateProgram");
+  glAttachShader = (PFNGLATTACHSHADERPROC)glXGetProcAddress((const GLubyte*)"glAttachShader");
+  glLinkProgram = (PFNGLLINKPROGRAMPROC)glXGetProcAddress((const GLubyte*)"glLinkProgram");
+  glUseProgram = (PFNGLUSEPROGRAMPROC)glXGetProcAddress((const GLubyte*)"glUseProgram");
+  glGenBuffers = (PFNGLGENBUFFERSPROC)glXGetProcAddress((const GLubyte*)"glGenBuffers");
+  glBindBuffer = (PFNGLBINDBUFFERPROC)glXGetProcAddress((const GLubyte*)"glBindBuffer");
+  glBufferData = (PFNGLBUFFERDATAPROC)glXGetProcAddress((const GLubyte*)"glBufferData");
+  glGetAttribLocation = (PFNGLGETATTRIBLOCATIONPROC)glXGetProcAddress((const GLubyte*)"glGetAttribLocation");
+  glVertexAttribPointer = (PFNGLVERTEXATTRIBPOINTERPROC)glXGetProcAddress((const GLubyte*)"glVertexAttribPointer");
+  glEnableVertexAttribArray = (PFNGLENABLEVERTEXATTRIBARRAYPROC)glXGetProcAddress((const GLubyte*)"glEnableVertexAttribArray");
+  glDisableVertexAttribArray = (PFNGLDISABLEVERTEXATTRIBARRAYPROC)glXGetProcAddress((const GLubyte*)"glDisableVertexAttribArray");
 
-    // Get the screen number
-    int screen = DefaultScreen(display);
+  float quadVertices[] = {-1.0f, -1.0f, 1.0f, -1.0f, -1.0f, 1.0f, 1.0f, 1.0f};
 
-    // Choose a framebuffer configuration
-    static int visualAttributes[] = {
-        GLX_X_RENDERABLE, True,
-        GLX_DRAWABLE_TYPE, GLX_WINDOW_BIT,
-        GLX_RENDER_TYPE, GLX_RGBA_BIT,
-        GLX_X_VISUAL_TYPE, GLX_TRUE_COLOR,
-        GLX_RED_SIZE, 8,
-        GLX_GREEN_SIZE, 8,
-        GLX_BLUE_SIZE, 8,
-        GLX_ALPHA_SIZE, 8,
-        GLX_DEPTH_SIZE, 24,
-        GLX_STENCIL_SIZE, 8,
-        GLX_DOUBLEBUFFER, True,
-        None
-    };
-
-    int fbCount;
-    GLXFBConfig *fbConfigs = glXChooseFBConfig(display, screen, visualAttributes, &fbCount);
-
-    // Get the X Visual info
-    GLXFBConfig fbConfig = fbConfigs[0];
-    XVisualInfo *visualInfo = glXGetVisualFromFBConfig(display, fbConfig);
-
-    // Create a colormap and set the window attributes
-    Window rootWindow = RootWindow(display, screen);
-    Colormap colormap = XCreateColormap(display, rootWindow, visualInfo->visual, AllocNone);
-
-    XSetWindowAttributes windowAttributes;
-    windowAttributes.colormap = colormap;
-    windowAttributes.background_pixel = WhitePixel(display, screen);
-    windowAttributes.border_pixel = 0;
-    windowAttributes.event_mask = ExposureMask | KeyPressMask;
-
-    Window window = XCreateWindow(
-        display, rootWindow,
-        0, 0, width, height, 0,
-        visualInfo->depth, InputOutput,
-        visualInfo->visual, CWColormap | CWBackPixel | CWBorderPixel | CWEventMask,
-        &windowAttributes
-    );
-
-    XStoreName(display, window, title);
-    XMapWindow(display, window);
-
-    // Create the GLX context
-    GLXContext context = glXCreateNewContext(display, fbConfig, GLX_RGBA_TYPE, nullptr, True);
-
-    XFree(visualInfo);
-
-    // Make the GLX context current
-    glXMakeCurrent(display, window, context);
+  GLuint vbo;
+  glGenBuffers(1, &vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), quadVertices, GL_STATIC_DRAW);
 
   /*
-  * ---10--------20--------30--------40--------50--------60--------70-------80
-  */
-  bool application_running = true;
-  XEvent event;
-  while (application_running) {
-    while (XPending(display)) {
-      XNextEvent(display, &event);
-      if (event.type == KeyPress) {
-        KeySym key = XLookupKeysym(&event.xkey, 0);
-        if (key == XK_Escape){
-          application_running = false;
-        }
-      }
-    }
-  }
+    * ---10--------20--------30--------40--------50--------60--------70-------80
+    */
+  GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+  glCompileShader(vertexShader);
 
+  GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+  glCompileShader(fragmentShader);
+
+  GLuint shaderProgram = glCreateProgram();
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glLinkProgram(shaderProgram);
 
   /*
-  * ---10--------20--------30--------40--------50--------60--------70-------80
-  */
-  glXDestroyContext(display, context);
+    * ---10--------20--------30--------40--------50--------60--------70-------80
+    */
+  while (1) {
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glUseProgram(shaderProgram);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    GLint positionAttribLocation = glGetAttribLocation(shaderProgram, "position");
+    glVertexAttribPointer(positionAttribLocation, 2, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(positionAttribLocation);
+
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+
+    glDisableVertexAttribArray(positionAttribLocation);
+
+    glXSwapBuffers(display, window);
+   }
+
+  /*
+    * ---10--------20--------30--------40--------50--------60--------70-------80
+    */
+  glXDestroyContext(display, glxContext);
+  XDestroyWindow(display, window);
   XCloseDisplay(display);
+
+
   return 0;
-}
+ }
 
 /*
-  * -----10--------20--------30--------40--------50--------60--------70-------80
-  */
+ * -----10--------20--------30--------40--------50--------60--------70-------80
+ */
